d76f1c5dc69a1631c723266ddefd4bcd5cefc948
LucasDNunes <lucasdomingosnunes@gmail.com>
2018-04-05 16:40:12 -0300

    .

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,15 +1,17 @@
 class Negociacao {
 	constructor(data, quantidade, valor) {
-		this._data = data;
+		
+		this._data = new Date(data.getTime()); // usando o new date para que nao possa ocorrer alteração
 		this._quantidade = quantidade;
 		this._valor = valor;
 
 		Object.freeze(this); // congelando objeto para nao poder ser alterado
 	}
-	//antes getVolume
+	//antes era getVolume
 	get volume(){ //criando atributos get nao sendo necessarios chamar getVolume() e sim apenas volume
 		return this._quantidade * this._valor; // o que tem _ nao é para acessar diretamente e sim por metodos
 	}
+
 	get data(){
 		return this._data;
 	}
@@ -21,8 +23,5 @@ class Negociacao {
 	get valor(){
 		return this._valor;
 	}
-
-
-
 
 }	// methods
