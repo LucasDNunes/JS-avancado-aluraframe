f283a0018b33d54eb8973ab9eb9ea5c9202e8238
LucasDNunes <lucasdomingosnunes@gmail.com>
2018-04-06 15:41:00 -0300

    Arrumando data e usando ES6
    Resolvendo problemas com data usando .map();
    Usando arrow functions do ES6.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -18,21 +18,28 @@ class NegociacaoController {
 		//console.log(typeof(this._inputData.value));
 
 		//transformando em array para reconhecer no new Date()
-		//console.log(this._inputData.value.split('-'));    - quebrando as string apartir do '-'
+		//console.log(this._inputData.value.split('-'));  //  - quebrando as string apartir do '-'
 		//console.log(this._inputData.value.replace(/-/g,','));   - trocando os '-' para ','
-		let data = new Date(this._inputData.value.split('-'));
+		// ... usando 'spread operator' para desmebrar o array
+		//.map() passando parametro o item do array e o indice do array, para diminuir o mes e mostrar a data certa
 
-		//arrumar data
+		// usando arrow functions do ES6
+		let data = new Date(...this._inputData.value
+									.split('-')
+									.map((item, indice) => item - indice % 2)
+									//nao precisa abrir {} apos => e da return pq só tem um linha de cod
+									//usando o % para pegar a posição do mês que é na posição 1
+
+		);
 
 		let negociacao = new Negociacao(
-			//this._inputData.value.split('-'),
-			//this._inputData.value.replace(/-/g,','),
 			data,
 			this._inputQuantidade.value,
 			this._inputValor.value
 		);
 
 		console.log(negociacao);
+
 	}
 
 }
