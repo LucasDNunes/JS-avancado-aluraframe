092cb5e119af3e597d3340fe01745bd1f971a345
LucasDNunes <lucasdomingosnunes@gmail.com>
2018-05-25 01:05:03 -0300

    criando ListModel

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,6 +6,7 @@ class NegociacaoController {
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
+		this._listaNegociacoes = new ListaNegociacoes();
 
 	}
 
@@ -14,18 +15,26 @@ class NegociacaoController {
 		event.preventDefault(); 
 
 		//let helper = new DateHelper();
-		let data = DateHelper.textoParaData(this._inputData.value);
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._limpaFormulario();
+		console.log(this._listaNegociacoes.negociacoes);
+		
+	}
 
-		let negociacao = new Negociacao(
-			data,
+	_limpaFormulario(){
+		this._inputData.value = '';
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0.0;
+
+		this._inputData.focus();
+	}
+
+	_criaNegociacao(){
+
+		return new Negociacao(
+			DateHelper.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
-			this._inputValor.value
-		);
-
-
-		console.log(negociacao);
-
-		console.log(DateHelper.dataParaTexto(negociacao.data));
+			this._inputValor.value);
 	}
 
 }
