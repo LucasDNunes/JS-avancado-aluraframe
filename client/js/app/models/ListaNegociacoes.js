7b80cd2da101b496ed33dcc6de3f0ae868f8d14f
LucasDNunes <lucasdomingosnunes@gmail.com>
2018-05-30 11:16:04 -0300

    .

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -9,6 +9,6 @@ class ListaNegociacoes{
     }
 
     get negociacoes(){
-        return this._negociacoes;
+        return [].concat(this._negociacoes); //retorna um novvo array impossibilitando de alterar o array original
     }
 }
